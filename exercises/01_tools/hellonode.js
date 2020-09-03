0884f38c5422b3fd5a8e6353825f8fb82275caa0
Joonas Pelttari <joonas.pelttari@tuni.fi>
2020-09-04 02:10:09 +0300

    Uusi yritys 1.4

--- exercises/01_tools/hellonode.js
+++ exercises/01_tools/hellonode.js
@@ -1,11 +1,12 @@
 const _ = require("lodash");
-
+const ver = _.VERSION;
+console.log(ver);    
 
 function hellonode(array)
 {
     let first_element = _.first(array);
     let last_element = _.last(array)
-    console.log('${first_element} ${last_element}');
+    console.log(first_element,last_element);
 }
 
 module.exports = hellonode
