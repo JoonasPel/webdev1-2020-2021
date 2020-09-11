a3db3fe4f05aed29ab0ca49992e340c809858b15
Joonas Pelttari <joonas.pelttari@tuni.fi>
2020-09-12 01:02:58 +0300

    2.3 tehty Echo

---
+++ exercises/02_http/2.3-echo-server/index.js
@@ -0,0 +1,19 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    console.log('req was made: ' + req.url);
+    res.writeHead(200, { 'Content-type' : 'text/plain' });
+
+    var body = [];
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+    });
+
+    res.write(body);
+    res.end();
+});
+
+server.listen(3000, 'localhost');
+console.log('Listening to 3000');
