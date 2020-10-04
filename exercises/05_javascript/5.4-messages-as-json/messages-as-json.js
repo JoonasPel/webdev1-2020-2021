89d39d2fe4ad3590f8100d8ec1272c63b0cad083
Joonas Pelttari <joonas.pelttari@tuni.fi>
2020-10-04 21:18:47 +0300

    5.4 turha indeksointi pois

--- exercises/05_javascript/5.4-messages-as-json/messages-as-json.js
+++ exercises/05_javascript/5.4-messages-as-json/messages-as-json.js
@@ -11,34 +11,37 @@ function data_received(event) {
     var arr = JSON.parse(event.detail.jsonText);
 
     var clone, temp_name, temp_email, temp_phone, temp_address, temp_homepage,
-    temp_avatar;
+    temp_avatar, arr_object;
     for (var i = 0; i < arr.length; i++) {
+
+        arr_object = arr[i];
 
         //clone template
         clone = template.content.cloneNode(true);
 
         //replace contents
         temp_name = clone.querySelector("h1");
-        temp_name.textContent = arr[i].firstName + " " + arr[i].lastName;
+        temp_name.textContent = arr_object.firstName + " " + arr_object.lastName;
 
         temp_email = clone.querySelector(".title.email");
-        temp_email.textContent = arr[i].email;
+        temp_email.textContent = arr_object.email;
 
         temp_phone = clone.querySelector(".phone > span");
-        temp_phone.textContent = arr[i].phoneNumber;
+        temp_phone.textContent = arr_object.phoneNumber;
 
         temp_address = clone.querySelectorAll(".address > p");
-        temp_address[0].textContent = arr[i].address.streetAddress;
-        temp_address[1].textContent = arr[i].address.zipCode+" "+arr[i].address.city;
-        temp_address[2].textContent = arr[i].address.country;
+        temp_address[0].textContent = arr_object.address.streetAddress;
+        temp_address[1].textContent = 
+        arr_object.address.zipCode + " " + arr_object.address.city;
+        temp_address[2].textContent = arr_object.address.country;
 
         temp_homepage = clone.querySelector(".homepage > a");
-        temp_homepage.textContent = arr[i].homepage;
-        temp_homepage.href = arr[i].homepage; 
+        temp_homepage.textContent = arr_object.homepage;
+        temp_homepage.href = arr_object.homepage; 
 
         temp_avatar = clone.querySelector("img");
-        temp_avatar.src = arr[i].avatar;
-        temp_avatar.alt = arr[i].firstName + " " + arr[i].lastName;
+        temp_avatar.src = arr_object.avatar;
+        temp_avatar.alt = arr_object.firstName + " " + arr_object.lastName;
 
         //append data
         div_contacts.appendChild(clone);
