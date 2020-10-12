294d38112791b45bc8ea066a19ae1e0b1a33aab7
Joonas Pelttari <joonas.pelttari@tuni.fi>
2020-10-12 23:34:07 +0300

    6.2 tehty

--- exercises/06_async/ex2/test/f.test.js
+++ exercises/06_async/ex2/test/f.test.js
@@ -2,15 +2,19 @@ const expect = require("chai").expect;
 
 const f = require("../f_skeleton");
 
-describe("thenable", () => {
-  it("resolves with correct value", () => {
     // TODO: Implement this test. See rejectable below as a model
     // See: https://www.testim.io/blog/testing-promises-using-mocha/
     // This test can be implemented with either Promises or with async / await.
     // Use expect() and to.equal()
     // to make sure that the resolved value is 👍
-    throw "Not yet implemented!";
-  });
+describe("thenable", () => {
+  it("resolves with correct value", (done) => {
+    Promise.resolve(f.thenable)
+    .then((result) => {     
+      expect(result).to.equal("👍");
+      done();
+    });
+  }); 
 });
 
 describe("rejectable", () => {
